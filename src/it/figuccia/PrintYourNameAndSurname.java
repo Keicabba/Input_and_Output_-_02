9b748ba8fec9b65e5e716f0fae28806b92d18b98
Keicabba <peppino_cbr@hotmail.com>
2023-01-27 02:39:44 +0100

    Initial commit

---
+++ src/it/figuccia/PrintYourNameAndSurname.java
@@ -0,0 +1,10 @@
+package it.figuccia;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("My name is Pinco ");
+        System.out.println("Pallino");
+    }
+}
+
+//come detto nell'esercizio prima, questo andava fatto nella prima settimana di corso.
